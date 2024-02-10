a8868207041ab4d559ca0658a8858e534ad813f9
nol0n <sharapovegor2003@gmail.com>
2024-02-10 21:53:29 +0300

    Added meaningful library example (shader helper class)

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <mylib.hpp>
+#include <shader.hpp>
+#include <assets.hpp>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
@@ -35,12 +37,50 @@ int main()
         return -1;
     }
 
+    //
+
+    float vertices[] = {
+        0.5f,  0.5f, 0.0f,  
+        0.5f, -0.5f, 0.0f,  
+        -0.5f, -0.5f, 0.0f, 
+        -0.5f,  0.5f, 0.0f  
+    };
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3 
+    };
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    Shader shader(SHADER_DIR "/vertex_shader.glsl", SHADER_DIR "/fragment_shader.glsl");
+    shader.use();
+
+    //
+
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
